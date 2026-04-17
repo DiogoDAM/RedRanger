204e1cfba9d832e15d1deaa8d89fa4277f393b76
DiogoDAM <diogo.andradem20@protonmail.com>
2026-04-17 12:38:24 -0300

    Create UiElement [not tested]

---
+++ Source/Engine/Ui/UiElement.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+namespace SapukayaEngine;
+
+public abstract class UiElement : IDisposable
+{
+	public bool Active;
+	public bool Visible;
+	public bool Alive;
+
+	public bool Disposed { get; private set; }
+
+	public bool CanUpdate => Active && Alive && !Disposed;
+	public bool CanDraw => Visible && Alive && !Disposed;
+
+	public List<UiElement> Children { get; protected set; }
+
+	public UiElement Parent { get; protected set; }
+
+	public Transform Transform { get; protected set; }
+
+	public float Gap { get; set; }
+
+	public virtual int Width { get; protected set; }
+	public virtual int Height { get; protected set; }
+
+	public UiElement()
+	{
+		Children = new();
+		Transform = new();
+	}
+
+	public void AddChild(UiElement child)
+	{
+		if(child == null)
+			throw new ArgumentNullException("UiElement child is null");
+
+		Children.Add(child);
+		child.Added(this);
+		child.Resume();
+		child.Start();
+	}
+
+	public void RemoveChild(UiElement child)
+	{
+		if(child == null)
+			throw new ArgumentNullException("UiElement child is null");
+
+		Children.Remove(child);
+		child.Pause();
+		child.Removed();
+	}
+
+	public virtual void Added(UiElement parent)
+	{
+		Parent = parent;
+		Transform.Parent = parent.Transform;
+	}
+
+	public virtual void Removed()
+	{
+		Parent = null;
+		Transform.Parent = null;
+	}
+
+	public virtual void Start()
+	{
+	}
+
+	public virtual void Update(float dt)
+	{
+		foreach(UiElement child in Children)
+		{
+			if(child.CanUpdate)
+				child.Update(dt);
+		}
+	}
+
+	public virtual void Draw()
+	{
+		foreach(UiElement child in Children)
+		{
+			if(child.CanDraw)
+				child.Draw();
+		}
+	}
+
+	public virtual void Resume()
+	{
+		Active = true;
+		Visible = true;
+		Alive  = true;
+	}
+
+	public virtual void Pause()
+	{
+		Active = false;
+		Visible = false;
+		Alive = false;
+	}
+
+	public void Dispose()
+	{
+		if(!Disposed)
+		{
+			Dispose(true);
+			Disposed = true;
+			GC.SuppressFinalize(this);
+		}
+	}
+
+	protected virtual void Dispose(bool disposable)
+	{
+		if(!disposable)
+			return;
+
+		foreach(UiElement child in Children)
+		{
+			child.Dispose();
+		}
+		Children.Clear();
+
+		Children = null;
+
+		Transform = null;
+	}
+}
--- obj/Debug/net9.0/RedRanger.AssemblyInfo.cs
+++ obj/Debug/net9.0/RedRanger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedRanger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c38eb7aa04e046b7fb028f3475681a71ad71b68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9503d68ccce7485f3a89f0e66e16a4db9eaa3c8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedRanger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedRanger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
